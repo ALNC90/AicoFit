a80b021adccfed2ab49de05f4626606a19fc224f
Alin Ioan Chelemen <alnc0090@outlook.es>
2023-06-13 13:50:37 +0200

    Update_7

--- login.php
+++ login.php
@@ -40,8 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
             {
                 session_start();
                 $_SESSION["user_id"]=$row['id'];
-                //header("Location:client_profile.php");
-                header("Location:https://aicofit.azurewebsites.net/client_profile.php");
+                echo "<script>window.location.href='client_profile.php';</script>";
             }
             else
             {
@@ -73,7 +72,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
                 session_start();
                 $_SESSION["user_id"]=$row['id'];
                 //header("Location:trainer_profile.php");  
-                header("Location:https://aicofit.azurewebsites.net/trainer_profile.php");
+                //header("Location:https://aicofit.azurewebsites.net/trainer_profile.php");
+                echo "<script>window.location.href='trainer_profile.php';</script>";
             }
             else
             {
