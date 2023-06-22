bec0b86d61d1101279864f2c4208d7a2579aaed4
Alin Ioan Chelemen <alnc0090@outlook.es>
2023-06-22 11:55:18 +0200

    Update_15

--- delete_routine.php
+++ delete_routine.php
@@ -6,7 +6,7 @@ if (isset($_POST["id"])) {
     $sql = "DELETE FROM routines WHERE id_rou = '$routine_id'";
     if (mysqli_query($connection, $sql)) {
         echo '<script>alert("Rutina eliminada correctamente.");</script>';
-        header('Location:trainer_profile.php');
+        echo "<script>window.location.href='trainer_profile.php';</script>";
     }
 }
 ?>
